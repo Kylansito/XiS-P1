f583f95ad6778ba2c57feeace3b98e8a6a862235
Kylansito <jcayla@edu.tecnocampus.cat>
2023-10-05 16:07:30 +0200

    Ip Scann

--- Practica1/Practica1.py
+++ Practica1/Practica1.py
@@ -5,6 +5,29 @@ from scapy.all import *
 #Generar paquete ICMP
 from scapy.all import IP, ICMP
 
-p = sr1(IP(dst="www.google.com")/ICMP()/"")
+allIp = []
+ttl = 0
+reached = False
+while ttl < 100 and not reached:
+    p = IP(dst="www.google.com", ttl=ttl)/ICMP()
+    ttl += 1
+    r = sr1(p, verbose=0, timeout=1)
+    if r is None:
+        print("Timeout")
+    else:
+        #print(r.src, "respondio con TTL: ", r.ttl)
+        print("TTL: ", ttl, "--> ", r.src)
+        if r.src not in allIp:
+            allIp.append(r.src)
+        if p.dst == r.src:
+            print("Llego al destino: ", r.src)
+            reached = True
 
-p.show()
+print("Direcciones IP encontradas: ", allIp)
+
+
+
+
+#p = sr1(IP(dst="www.google.com")/ICMP()/"")
+
+#p.show()
